d02cf82dd94fd7dfbbb827fc599b965b4eac64ac
mariashubna <mariashubna@gmail.com>
2023-10-21 15:43:53 +0200

    ./

---
+++ src/favorites.js
@@ -0,0 +1,5 @@
+import "./js/header-menu";
+import "./js/quote";
+import "./js/markup-favorites";
+import "./js/modal-exercise";
+import "./js/modal-rating";
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./js/header-menu";
+import "./js/exercises";
+import "./js/quote";
+import "./js/footer-form";
+import "./js/api-service";
+import "./js/favorites";
+import "./js/markup-categories";
+import "./js/markup-workouts";
+
+
